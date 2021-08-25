a47c44d4aa4665c3891bb3b06bd9613535439101
GregoryNieldApriant <89514666+GregoryNieldApriant@users.noreply.github.com>
2021-08-25 17:01:09 +0800

    Add files via upload

---
+++ Tugas_hal_22.m
@@ -0,0 +1,98 @@
+%1. 
+>> 12/3.5, (3+5/4)^2
+ans =
+3.4286
+ans =
+18.0625
+>> (.25^2 + .75^2)^(1/2), 2/(6/.3)
+ans =
+0.7906
+ans =
+0.1000
+
+%2. 
+>> A=25, B=50, C=125, D=89
+A =
+25
+B =
+50
+C =
+125
+D =
+89
+>> X = A+B+C, Y = A/(D+B)
+X =
+200
+Y =
+0.1799
+>> Z = D^(A/B) + C
+Z =
+134.4340
+
+%3. 
+luas : valid, kel_1 : valid,
+2_data : tidak valid, karena diawali dengan angka,
+diff:3 : tidak valid, karena mengandung titik-dua,
+Time : valid, time : valid, time_from_start : valid,
+10_hasil_terakhir : tidak valid, karena diawali dengan angka,
+nilai-awal : tidak valid, karena mengandung tanda minus
+
+%4. 
+>> x=pi/6; y=.001;
+>> sqrt(y), exp(-x), sin(x)
+ans =
+0.0316
+ans =
+0.5924
+ans =
+0.5000
+>> cos(2*x), tan(3*x)
+ans =
+0.5000
+ans =
+1.6331e+016
+>> log10(y), log2(y), log(y)
+ans =
+-3
+ans =
+-9.9658
+ans =
+-6.9078
+
+%5. 
+>> clear
+>> p = 9 + 16*i; q = -9 + 16*i;
+>> r=p*q, s=p/q, p-r
+r =
+-337
+s =
+0.5193 - 0.8546i
+ans =
+3.4600e+002 +1.6000e+001i
+>> r+s, p^2, sqrt(q)
+ans =
+-3.3648e+002 -8.5460e-001i
+ans =
+-1.7500e+002 +2.8800e+002i
+ans =
+2.1630 + 3.6985i
+>> abs(p), angle(p)
+ans =
+18.3576
+ans =
+1.0584
+>> abs(q), angle(q)
+ans =
+18.3576
+ans =
+2.0832
+>> abs(r), angle(r)
+ans =
+337
+ans =
+3.1416
+>> abs(s), angle(s)
+ans =
+1
+ans =
+-1.0248
